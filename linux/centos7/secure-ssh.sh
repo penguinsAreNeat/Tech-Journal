d87a694c4e1e1a0239ef2b03f5e324537e742cc5
penguinsAreNeat <jonathan.stroop@mymail.champlain.edu>
2023-02-20 11:39:11 -0500

    added secure-ssh.sh file

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#authoer: Jonathan Stroop
+#creates a new ssh user using $1 param
+#adds a public key from local repo or curled from remote repo
+#removes root ability to ssh in
+echo "code goes here"
