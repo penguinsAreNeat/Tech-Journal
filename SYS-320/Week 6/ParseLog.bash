5e37363be36051aefc3b4324f05985d1d8c44eaf
penguinsAreNeat <89825496+penguinsAreNeat@users.noreply.github.com>
2023-10-10 03:10:15 -0400

    Update ParseLog.bash
    Finalized assignment version

--- SYS-320/Week 6/ParseLog.bash
+++ SYS-320/Week 6/ParseLog.bash
@@ -4,6 +4,7 @@
 
 #Check if Apache log exists
 read -p "What is the Apache log file name: " logName
+
 if [ -f $logName ]
 then
 	echo "File exists"
@@ -12,12 +13,23 @@ else
 	exit 1
 fi
 
+# Get IPv4 addresses, sort out duplicates and put them in a temporary file
 awk '{print $1}' $logName | sort -u > uniqueIPs.txt
 echo "successful run"
 
-for eachip in $(cat  uniqueIPs.txt)
+# Discarded code 
+#for eachip in $(cat  uniqueIPs.txt)
+#do
+#	echo "sudo ufw deny from $eachip" > `tty`
+#done
+
+# Put each IPv4 address in a IPtables block rule
+for eachip in $(cat uniqueIPs.txt)
 do
-	echo "sudo ufw deny from $eachip" > `tty`
+	echo "iptables -a input -s ${eachip} -j drop" | tee -a  badips.iptables
 done
 
+echo "Created IPTables firewall drop rules in file \"badips.iptables\""
+
+# Delete tempoary file
 rm uniqueIPs.txt
