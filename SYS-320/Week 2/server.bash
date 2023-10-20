8f61c0101ec618b7aa200465fbb263dd538b8093
penguinsAreNeat <89825496+penguinsAreNeat@users.noreply.github.com>
2023-10-19 21:23:26 -0400

    Update server.bash

--- SYS-320/Week 2/server.bash
+++ SYS-320/Week 2/server.bash
@@ -1,82 +1,38 @@
-
 #!/bin/bash
 
-# Storyline: Script to create a wireguard server
-echo "Test for class code, Run for student code"
-read -p "Enter option: " varname
+# Storyline: Wiregaurd server creation script
 
-# Check if file exists, with a choice between example and student method
-if [ "$varname" == "Test" ]; then 
-  #echo "success"
-  #Example method
-
-  if [[ -f "wg0.conf" ]]
-  then 
-	  # Prompt if we need to overwrite the file
-    echo "The file wg0.conf already exists."
-	  echo -n "Do you want to overwrite it? [y|N]"
-	  read to_overwrite
-
-	  if [[ "${to_overwrite}" == "N" || "${to_overwrite}" == "" || "${to_overwrite}" == "n"  ]]
-	  then
-		  echo "Exiting..."
-		  exit 0
-	  elif [[ "${to_overwrite}" == "y" ]]
-	  then
-		  echo "Creating the wireguard configuration file..."
-	  # If they don't specify y/N then error
-	  else
-		  echo "Invalid value"
-		  exit 1
-	  fi
-  fi
-  
-elif [ "$varname" == "Run" ]; then 
-  #echo "other success"
-  #Student method
-
-  if test -f "wg0.conf"
-  then
-  read -p "The file wg0.conf aleady exists, do you want to overwrite it? [Y/N]" overwrite_choice
-  if [ "$overwrite_choice" == "Y" ]; then 
-  else exit 0     
-  fi
-  
-else echo "Typo/other error, please restart"
+#Check if config file exists
+if [test -f "wg0.conf"]
+then
+	echo "Config file already exists, overwrite? [Y/N]"
+ 	read response
+  	if [[ "${response}" == "N" ]]
+   	then
+    		exit 0
+      	elif [[ "${response}" == "Y" ]]
+       	then 
+		echo "File overwritten"
+  	else
+   		echo "Invalid input"
+     		exit 1
+       fi
 fi
 
-# Create a private key
-p="$(wg genkey)"
-echo "${p}" > /etc/wireguard/server_private.key
+#Generate Private Key
+privKey="$(wg genkey)"
 
-# Create a public key
-pub="$(echo ${p} | wg pubkey)"
-echo "${pub}" > /etc/wireguard/server_public.key
+#Generate Public Key
+pubKey="$(echo ${privKey} | wg pubkey)"
 
-# Set the addresses
-address="10.254.132.0/24"
+#Set Address
+IPv4Address="192.168.1.1/24,192.168.4.2/24"
 
-# Set Server IP Addresses
-ServerAddress="10.254.132.1/24"
+#Set Server Address
+ServerIPv4Address="192.168.10.1/24,192.168.10.2/24"
 
-# Set a listening port
-lport="4282"
+#Set Listening Port
+listeningPort="4282"
 
-# Info to be used in client configuration
-peerInfo="# ${address} 192.168.241.131:4282 ${pub} 8.8.8.8,1.1.1.1 1280 120 0.0.0.0/0"
-# 1: #, 2: For obtaining an IP address for each client.
-# 3: Server's actual IP address
-# 4: clients will need server public key
-# 5: dns information
-# 6: determines the largest packet size allowed
-# 7: keeping connection alive for
-# 8: what traffic to be routed through VPN
-
-echo "${peerInfo}
-[Interface]
-Address = ${ServerAddress}
-PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o ens33 -j MASQUERADE
-ListenPort = ${lport}
-PrivateKey = ${p}
-" > wg0.conf 
+#Client Config Options
+options="# ${IPv4Address} 192.168.100.10:${listeningPort} 8.8.8.8,1.1.1.1 ${pubKey}
