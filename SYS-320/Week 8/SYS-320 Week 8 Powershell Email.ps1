8555e8bb34a6158787e676d4545caa41b63d5ee2
penguinsAreNeat <89825496+penguinsAreNeat@users.noreply.github.com>
2023-10-24 00:50:35 -0400

    Add files via upload

---
+++ SYS-320/Week 8/SYS-320 Week 8 Powershell Email.ps1
@@ -0,0 +1,19 @@
+﻿# Storyline: Send email
+
+# Email Body
+
+# Assigns variable value
+$mg = "Hello there."
+# Assigns variable text color
+write-host -BackgroundColor Red -ForegroundColor while $msg
+
+# Email address
+$email = "jonathan.stroop@mymail.champlain.edu"
+
+# To address
+$toEmail = "deployer@csi-web"
+
+# Send Email
+Send-MailMessage -From $email -To $toEmail -Subject "Powershell Email Script Test" -SmtpServer 192.168.6.71
+
+# Variables must start with dollar sign
---
+++ SYS-320/Week 8/SYS-320 Week 8 Powershell Log Script.ps1
@@ -0,0 +1,31 @@
+﻿# Storyline: Review Security Log
+
+# Save file location
+$folder = "C:\Users\jonpe\Downloads\"
+
+# List all event lots
+                   #Test Code
+Get-EventLog -list #|select Log
+
+# Create prompt to select log view
+$readLog = Read-host -Prompt "Please select a log to check from the list"
+
+# Print log
+Get-EventLog -LogName $readLog -Newest 40 | export-csv -NoTypeInformation -Path "$folder\securityLog.csv"
+
+# Check if user wants to filter search
+$searchBool = Read-host -Prompt "Do you want to filter your search? [Y/N]"
+
+# Filter search
+if ($searchBool -eq "Y")
+{
+    # Get search term and filter logs
+    $searchTerm = Read-host -Prompt "What value do you want to filter by"
+    Get-EventLog -LogName $readLog -Newest 100 | Where-Object {$_.Message -match "$searchTerm"}
+
+    # Check if user wants to save filtered output
+    $printBool = Read-host -Prompt "Save output? [Y/N]"
+    if ($printBool -eq "y")
+       { Get-EventLog -LogName $readLog -Newest 100 | Where-Object {$_.Message -match "$searchTerm"} | export-csv -Path "$folder\SecurityLogFiltered.csv"}
+}
+echo "Ending script"
